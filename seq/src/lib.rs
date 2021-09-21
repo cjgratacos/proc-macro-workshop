b577fc454b0201b7146ad79d5f2b78e0e5842610
Carlos Gratacos <carlos.gratacos@payzer.com>
2021-09-20 22:23:49 -0400

    updates

--- seq/src/lib.rs
+++ seq/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, DeriveInput};
+
 #[proc_macro]
 pub fn seq(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
